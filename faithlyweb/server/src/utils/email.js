977504dee736163d61bb8d53aaeddbea4cc8abc0
mjmjmj3 <marcjoefreal@gmail.com>
2026-03-15 01:25:25 +0800

    Update email.js

--- faithlyweb/server/src/utils/email.js
+++ faithlyweb/server/src/utils/email.js
@@ -4,14 +4,14 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // use SSL
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
   tls: {
-    rejectUnauthorized: false // Helps in some cloud environments
+    rejectUnauthorized: false
   }
 });
 
