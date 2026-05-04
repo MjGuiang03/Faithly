aca66cfbb9e4b8f0dd0682dc1765e2cfbb726cbd
mjmjmj3 <marcjoefreal@gmail.com>
2026-05-04 22:44:03 +0800

    clean up

--- server/check_db.js
+++
@@ -1,6 +0,0 @@
-import { attendance } from './src/config/db.js';
-
-const records = await attendance.find({ sessionId: 'SESS-2026-0005' }).toArray();
-console.log('Records for SESS-2026-0005:', JSON.stringify(records, null, 2));
-console.log('Total:', records.length);
-process.exit(0);
